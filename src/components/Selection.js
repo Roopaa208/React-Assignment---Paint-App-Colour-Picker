c36455b56f8150fadc73c29516f39cae05012a97
Anurag Sahu <anuragsahu060197@gmail.com>
2021-02-17 15:46:54 +0530

    commited

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/Child.css';
 
 const Selection = ({applyColor}) => {
-    const [bg, setbg] = useState({background : ''});
+    const bg = useState({background : ''})[0];
     
     return (
         <div className="fix-box" style={bg} onClick={applyColor}>
